f3f0fbb84cbf76b2b76b2810e8d27d9b65867f28
JackKarichkovskiy <karichkovsky@yandex.com>
2016-05-31 13:55:49 +0300

    #goodtest1 with config files

--- src/main/java/edu/kpi/fiot/ot/test/SimulationClass.java
+++ src/main/java/edu/kpi/fiot/ot/test/SimulationClass.java
@@ -124,9 +124,9 @@ public class SimulationClass {
 	private List<Service> constructServices(){
 		List<Service> services =  new ArrayList<Service>() {
 			{
-				//add(new Service("Video buffering2", 30, new UniformGenerator(0.01, 0.02)));//50-100
-				add(new Service("Video buffering1", 10, new UniformGenerator(0.0333, 0.2)));//5-30
-				add(new Service("Video buffering2", 110, new UniformGenerator(0.009, 0.0111)));//90-110
+				add(new Service("Video buffering1", 30, new UniformGenerator(0.008, 0.013)));//75-125
+				add(new Service("Video buffering2", 30, new UniformGenerator(0.008, 0.013)));//75-125
+				add(new Service("Video buffering3", 30, new UniformGenerator(0.008, 0.013)));//75-125
 				//add(new Service("Weather service", 10, new UniformGenerator(0.0083, 0.0125)));//80-120
 				//add(new Service("Video buffering", 30, new UniformGenerator(0.005, 0.00667)));//150-200
 				//add(new Service("Email service", 100, new UniformGenerator(0.00143, 0.002))); //500-700
